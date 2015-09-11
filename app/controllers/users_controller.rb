15880adc8df7fa7bb08e2784ecbac11102e14ddf
ayaka010816 <ayaka.hostos@gmail.com>
2015-09-11 08:22:33 +0000

    add biography

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,48 @@
 class UsersController < ApplicationController
+   before_action :set_user,       only: [:show, :edit, :update, :destroy]
   
   def show # 追加
-   @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
   end
 
-def create
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # ここを修正
     else
       render 'new'
-    end
-end
+   end
+   end
+ end
+    
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+    def update
+    if @user.update(user_params)
+      flash[:success] = "Update your profile"
+      # 保存に成功した場合はユーザーページへリダイレクト
+      redirect_to edit_user_path , notice: 'プロフィールを更新しました'
+    else
+      # 更新に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
    private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:biography, :location)
   end
+
+def set_user
+  @user = User.find(params[:id])
+  
 end
