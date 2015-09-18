9f9e3fe61a44d116495c12087deaa75735e59e47
ayaka010816 <ayaka.hostos@gmail.com>
2015-09-18 14:29:23 +0000

    migrate2

--- db/migrate/20150909202922_create_users.rb
+++ db/migrate/20150909202922_create_users.rb
@@ -4,9 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
-      t.string :locatopn
-      t.string :biography
-     
 
       t.timestamps null: false
 
---
+++ db/migrate/20150918115823_add_location_biography_to_user.rb
@@ -0,0 +1,6 @@
+class AddLocationBiographyToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :location, :string
+    add_column :users, :biography, :string
+  end
+end
