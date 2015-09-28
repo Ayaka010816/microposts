f00957b5dcf5a68f5192e50c767136205f876d4b
ayaka010816 <ayaka.hostos@gmail.com>
2015-09-28 11:34:14 +0000

    image validates

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,9 +40,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
   # For images you might use something like this:
   # def extension_white_list
   #   %w(jpg jpeg gif png)
